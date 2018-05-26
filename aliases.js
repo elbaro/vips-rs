7322adb6996a4200c29239e4a6f9c4f1e8a1e503
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-26 07:45:32 +0000

    Deploy elbaro/vips-rs to github.com/elbaro/vips-rs.git:gh-pages

--- aliases.js
+++ aliases.js
@@ -1,6 +1,9 @@
 var ALIASES = {};
 ALIASES['lazy_static'] = {};
+ALIASES['lazy_static'] = {};
 ALIASES['vips'] = {};
 ALIASES['vips'] = {};
 ALIASES['vips'] = {};
+ALIASES['vips'] = {};
+ALIASES['vips_sys'] = {};
 ALIASES['vips_sys'] = {};
